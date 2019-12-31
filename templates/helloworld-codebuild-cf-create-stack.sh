b8161c027f673c9b852ceedcd90cf49cc1c7f6fc
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-12-31 22:42:40 +0900

    Print /tmp/build.json

--- templates/helloworld-codebuild-cf-create-stack.sh
+++ templates/helloworld-codebuild-cf-create-stack.sh
@@ -2,7 +2,7 @@
 
 python helloworld-codebuild-cf-template.py > helloworld-codebuild-cf.template
 
-aws cloudformation create-stack \
+aws cloudformation update-stack \
 --stack-name helloworld-codebuild \
 --capabilities CAPABILITY_IAM \
 --template-body file://helloworld-codebuild-cf.template
