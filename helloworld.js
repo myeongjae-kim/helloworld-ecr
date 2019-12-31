99b8d4afbb12165e853a7aa9507a7db2d9bf7138
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-12-31 16:54:53 +0900

    Update message.

--- helloworld.js
+++ helloworld.js
@@ -1,8 +1,8 @@
 var http = require("http")
 
 http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello, world!\n')
+  response.writeHead(200, { 'Content-Type': 'text/plain' })
+  response.end('Hello, world again!\n')
 }).listen(3000)
 
 console.log('Server running')
