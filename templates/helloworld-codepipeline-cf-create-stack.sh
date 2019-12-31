493bb997482cff31e016163fed3ff3404c42dbba
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-12-31 16:37:25 +0900

    Adding Pipeline to deploy our helloworld application using ECS

---
+++ templates/helloworld-codepipeline-cf-create-stack.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+python helloworld-codepipeline-cf-template.py > helloworld-codepipeline-cf.template
+
+aws cloudformation create-stack \
+--stack-name helloworld-codepipeline \
+--capabilities CAPABILITY_NAMED_IAM \
+--template-body file://helloworld-codepipeline-cf.template
