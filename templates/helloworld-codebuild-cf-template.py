982f9cd1310d9e26aef9ce82f4afcae29e24199a
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2020-01-01 23:44:20 +0900

    Update code build.

--- templates/helloworld-codebuild-cf-template.py
+++ templates/helloworld-codebuild-cf-template.py
@@ -70,11 +70,13 @@ phases:
       - aws codepipeline get-pipeline-state --name "${CODEBUILD_INITIATOR##*/}" --query stageStates[?actionStates[0].latestExecution.externalExecutionId==\`$CODEBUILD_BUILD_ID\`].latestExecution.pipelineExecutionId --output=text > /tmp/execution_id.txt
       - aws codepipeline get-pipeline-execution --pipeline-name "${CODEBUILD_INITIATOR##*/}" --pipeline-execution-id $(cat /tmp/execution_id.txt) --query 'pipelineExecution.artifactRevisions[0].revisionId' --output=text > /tmp/tag.txt
       - printf "%s:%s" "$REPOSITORY_URI" "$(cat /tmp/tag.txt)" > /tmp/build_tag.txt
-      - printf '{"tag":"%s", "node_env":"%s"}' "$(cat /tmp/tag.txt)" | tee /tmp/build.json $NODE_ENV_JSON
+      - printf '{"tag":"%s", "node_env":"%s"}' "$(cat /tmp/tag.txt)" $NODE_ENV_JSON | tee /tmp/build.json
       - $(aws ecr get-login --no-include-email)
   build:
     commands:
+      - echo $BROWSER_ENV_JSON
       - BROWSER_ENV=$(echo $BROWSER_ENV_JSON | jq -r "to_entries|map(\\"\\(.key)=\\(.value|tostring)\\")|.[]" | tr "\\n" " ")
+      - echo $BROWSER_ENV
       - docker build --build-arg browser_env=$BROWSER_ENV -t "$(cat /tmp/build_tag.txt)" .
   post_build:
     commands:
