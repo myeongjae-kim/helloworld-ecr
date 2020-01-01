e371060addfe6ae824a4552922ca21c392f37773
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2020-01-02 01:18:54 +0900

    Update pipeline.

--- templates/helloworld-codepipeline-cf-create-stack.sh
+++ templates/helloworld-codepipeline-cf-create-stack.sh
@@ -2,7 +2,7 @@
 
 python helloworld-codepipeline-cf-template.py > helloworld-codepipeline-cf.template
 
-awsecr cloudformation create-stack \
+aws cloudformation create-stack \
 --stack-name helloworld-codepipeline \
 --capabilities CAPABILITY_NAMED_IAM \
 --template-body file://helloworld-codepipeline-cf.template
